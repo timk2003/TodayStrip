89dd310a6d21c8ce2506feab6c3ae8b1961ff9ea
timk2003 <t.krisch03@gmail.com>
2026-08-20 22:28:50 +0200

    Initial Commit

---
+++ TodayStrip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodayStrip
+//
+//  Created by Tim Krisch on 20.08.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodayStrip/TodayStripApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayStripApp.swift
+//  TodayStrip
+//
+//  Created by Tim Krisch on 20.08.26.
+//
+
+import SwiftUI
+
+@main
+struct TodayStripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
